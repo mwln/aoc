78b50ffc07d356e8f37526de77ee54651583a527
Marty Whelan <marty.whelan@verafin.com>
2022-12-06 12:55:16 -0330

    cleanup

--- src/years/y22/d6.rs
+++ src/years/y22/d6.rs
@@ -7,11 +7,10 @@ fn solve(part: aoc::AnswerPart) {
         aoc::AnswerPart::One => 4,
         aoc::AnswerPart::Two => 14,
     } as usize; 
-    let characters: Vec<char> = input.chars().collect();
-    'subroutine: for start in 0..characters.len() - 4 {
-        let stream = &characters[start..start+end];
+    'subroutine: for start in 0..input.len() - 4 {
+        let stream = &input[start..start+end];
         let mut unique = HashSet::new();
-        if stream.into_iter().all(|x| unique.insert(x)) {
+        if stream.chars().all(|x| unique.insert(x)) {
             println!("{}", start + end);
             break 'subroutine;
         }
