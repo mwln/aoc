dbc4c7e14b0a9b10918cb2eec686c9a6fca40878
Marty Whelan <marty.whelan@verafin.com>
2022-12-06 14:21:00 -0330

    spice

--- src/years/y22/d6.rs
+++ src/years/y22/d6.rs
@@ -1,20 +1,21 @@
 use aoc;
-use std::collections::HashSet;
+use itertools::Itertools;
 
 fn solve(part: aoc::AnswerPart) {
     let input = aoc::get_input(aoc::AdventDate { year: 2022, day: 6 });
-    let end = match part {
+    let size = match part {
         aoc::AnswerPart::One => 4,
         aoc::AnswerPart::Two => 14,
-    } as usize; 
-    'subroutine: for start in 0..input.len() - 4 {
-        let stream = &input[start..start+end];
-        let mut unique = HashSet::new();
-        if stream.chars().all(|x| unique.insert(x)) {
-            println!("{}", start + end);
-            break 'subroutine;
-        }
-    }
+    } as usize;
+    println!(
+        "{}",
+        input
+            .as_bytes()
+            .windows(size)
+            .position(|b| b.iter().unique().count() == size)
+            .unwrap()
+            + size,
+    );
 }
 
 pub(crate) fn solution(part: aoc::AnswerPart) {
