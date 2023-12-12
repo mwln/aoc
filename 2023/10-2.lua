d393ae61ee972fc9b5d6e5eb9ae6ec251e8222c8
Marty Whelan <marty.whelan@verafin.com>
2023-12-12 08:25:38 -0330

    almost day 10 part 2

--- 2023/10-2.lua
+++ 2023/10-2.lua
@@ -1,9 +1,12 @@
 local maze = {}
+local dmap = {}
 local px, py = -1, -1
 for line in io.lines("input.txt") do
     local row = {}
+    table.insert(dmap, {})
     for tile in line:gmatch(".") do
         table.insert(row, tile)
+        table.insert(dmap[#dmap], ".")
         if tile == "S" then
             px, py = #row, #maze + 1
         end
@@ -14,9 +17,9 @@ end
 local moves = { up = 1, down = 2, right = 3, left = 4 }
 local started = false
 local last_move = nil
-local steps = 0
 while true do
     local current = maze[py][px]
+    dmap[py][px] = "="
     local down, left, right = nil, nil, nil
     if current == "S" then
         if started then
@@ -94,7 +97,62 @@ while true do
             last_move = moves.left
         end
     end
-    steps = steps + 1
 end
 
-print(steps / 2)
+local function border(original)
+    local height = #original
+    local width = #original[1]
+    local bordered = {}
+    local top_bottom = {}
+    for _ = 1, width + 2 do
+        table.insert(top_bottom, ".")
+    end
+    table.insert(bordered, top_bottom)
+    for i = 1, height do
+        local rowWithBorder = { "." }
+        for j = 1, width do
+            table.insert(rowWithBorder, original[i][j])
+        end
+        table.insert(rowWithBorder, ".")
+        table.insert(bordered, rowWithBorder)
+    end
+    table.insert(bordered, top_bottom)
+    return bordered
+end
+
+
+local bmap = border(dmap)
+local to_visit = {}
+table.insert(to_visit, { 1, 1 })
+while #to_visit > 0 do
+    local x, y = table.unpack(table.remove(to_visit))
+    if bmap[y][x] ~= "=" and bmap[y][x] ~= "0" then
+        bmap[y][x] = "0"
+        for dy = -1, 1 do
+            for dx = -1, 1 do
+                local ny = y + dy
+                local nx = x + dx
+                if nx >= 1 and ny <= #bmap and nx <= #bmap[1] and ny >= 1 then
+                    table.insert(to_visit, { nx, ny })
+                end
+            end
+        end
+    end
+end
+
+local function can_squeeze(x, y)
+    local lx, ly = x - 1, y
+    local ux, uy = x, y - 1
+    local dx, dy = x, y + 1
+    local rx, ry = x + 1, y
+end
+
+local enclosed = 0
+for _, row in ipairs(bmap) do
+    for _, val in ipairs(row) do
+        if val == "." then
+            enclosed = enclosed + 1
+        end
+    end
+end
+print(enclosed)
